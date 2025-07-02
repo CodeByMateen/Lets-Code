35b6d8ec50d2c6aae66cd30295be536141404092
Mateen Shahzad <142880053+CodeByMateen@users.noreply.github.com>
2025-07-02 11:28:28 +0500

    Update 01_sum_of_even_numbers.py

--- Hands On Python/01_sum_of_even_numbers.py
+++ Hands On Python/01_sum_of_even_numbers.py
@@ -10,7 +10,7 @@ print(evenSum(nums))
 
 # sol 2 using filter and lambda
 
-even_nums = filter(lambda x: x % 2 == 0, nums)
+even_nums = sum(filter(lambda x: x % 2 == 0, nums))
 print(sum(even_nums))
 
 # sol 3 using list comprehension
